e02db03dc8d6ce2c63287722cc70e2aabaaa2f78
Juna Kim <kimjuna0118@gmail.com>
2024-06-14 15:00:04 -0400

    section divide

---
+++ calendar/app/index.jsx
@@ -0,0 +1,82 @@
+import { Text, View, Image, StyleSheet, Platform } from 'react-native';
+
+// import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+
+
+export default function HomeScreen() {
+  return (
+    <View>
+        <View style = {styles.reactLogo}>
+          <Text>
+            June 14, 2024
+          
+          <Text style = {styles.percentage}>
+            100%
+          </Text></Text>
+            </View>
+      
+      <View style={styles.calendar}>
+        <View style={styles.todo}>
+      </View>
+      </View>
+
+      <View style={styles.weekly}>
+      </View>
+
+
+
+
+
+
+
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    marginTop: 50,
+    marginLeft: 20,
+  },
+  percentage: {
+    textAlign: "right",
+    fontSize: 70,
+    fontFamily: "Courier New",
+    fontWeight: "bold",
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 150,
+    width: 390,
+    top: 40,
+    marginBottom: 45,
+  },
+
+  calendar: {
+    height: 520,
+    width: 180,
+    backgroundColor: 'blue',
+  },
+  todo: {
+    marginLeft: 182,
+    height: 520,
+    width: 208,
+    backgroundColor: 'red',
+  },
+  weekly: {
+    height: 80,
+    width: 390,
+    backgroundColor: 'grey',
+    marginTop: 10,
+  }
+
+});
